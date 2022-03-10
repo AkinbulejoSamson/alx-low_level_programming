5bbfb006a01bbb4247faaefb07d832715d4c6ac7
Akinbulejo Samson <akinbulejosamson77@gmail.com>
2022-03-10 05:33:23 -0800

    added printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - Prints a sentence
+ *
+ * Return - 0 if exited propely , non zero if otherwise
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
