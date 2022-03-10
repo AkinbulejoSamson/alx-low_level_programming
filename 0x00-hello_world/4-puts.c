11fdefe5c5eb03dd52611cbc77caa837d8859217
Akinbulejo Samson <akinbulejosamson77@gmail.com>
2022-03-10 05:25:49 -0800

    added puts,c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
 
-int main(void)
+/**
+ * main - Prints a phrase
+ *
+ * Return: 0 if exited properly, non-zero otherwise
+ */
+
+void main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
-/**
- * main - Prints a phrase
- *
- * Return - 0 if exited properly, non-zero otherwise
- */
