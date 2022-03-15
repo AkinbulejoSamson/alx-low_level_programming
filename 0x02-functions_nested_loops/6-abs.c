ace75265ca0a870a16a9b23544fa25ee08498ce9
Akinbulejo Samson <akinbulejosamson77@gmail.com>
2022-03-15 11:20:05 -0700

    added 6-abs.c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _abs - computes absolute value
+ * @n: integer to be checked
+ *
+ * Return: the absolute value of the int
+ */
+
+int _abs(int)
+{
+	if (n >= 0)
+		return (n);
+	else
+		return (-n);
+}
