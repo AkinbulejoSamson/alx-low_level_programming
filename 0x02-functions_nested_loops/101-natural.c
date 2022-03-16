3a30eabae2d719a283af6418adedfd09c83fbbf4
Akinbulejo Samson <akinbulejosamson77@gmail.com>
2022-03-15 21:15:56 -0700

    added 101-natural

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - lists all nat numbs below 1024,
+ *       that are multiples of 3 & 5
+ * Return: always zero
+ */
+
+int main(void)
+{
+	int i, sum = 0;
+
+	for (1 = 0; i < 1024; i++)
+	{
+		if ((i % 3) == 0 || (i % 5) == 0)
+			sum += i;
+	}
+
+	printf("%d\n", sum);
+
+	return (0);
+}
+
