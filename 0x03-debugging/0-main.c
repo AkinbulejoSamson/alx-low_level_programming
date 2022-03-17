fcdedb29f3ebfb556b0d038f89ac19bf4cb39ff6
Akinbulejo Samson <akinbulejosamson77@gmail.com>
2022-03-17 13:17:12 -0700

    corr

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -2,16 +2,15 @@
 
 /**
  * main - tests function
- *
  * Return: 0
  */
 
 int main(void)
 {
-	int i;
+int i;
 
-	i = 0;
-	positive_or_negative(i);
+i = 0;
+positive_or_negative(i);
 
-	return (0);
+return (0);
 }
