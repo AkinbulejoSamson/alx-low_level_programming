2e4d725d68748892093ec93abb6399c925c4a627
Akinbulejo Samson <akinbulejosamson77@gmail.com>
2022-03-21 07:04:16 -0700

    added Task 4

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -7,10 +7,8 @@
 
 void print_rev(char *s)
 {
-	int len = 0, index;
-
-	while (s[index++])
-		len++;
+	int len = _strlen(s);
+	int index;
 
 	for (index = len - 1; index >= 0; index--)
 		_putchar(s[index]);
