2af6a8b96d341755124b84d4785fcf883fea5c3d
Akinbulejo Samson <akinbulejosamson77@gmail.com>
2022-03-28 11:14:54 -0700

    added Task 1

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _memcpy - copies memory data from src to dest
+ * @dest: destination pointer
+ * @src: Pointer tk source
+ * @n: number of bytes to be copied
+ *
+ * Return: pointer to dest
+ */
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int counter;
+
+	for (counter = 0; counter < n; counter++)
+	{
+		dest[counter] = src[counter]
+	}
+
+	return (dest);
+}
