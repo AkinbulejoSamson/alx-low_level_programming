a4f9227ce140a822612f6c79caa0518b07dd9369
Akinbulejo Samson <akinbulejosamson77@gmail.com>
2022-04-01 11:24:17 -0700

    added Task 0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main- prints its name followed by new line
+ * @argc: number of args supplied
+ * @argv: array of pointers to args
+ *
+ * Return: always 0
+ */
+
+int main(int __attribute__((__unused__)) argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+
+	return (0);
+}
