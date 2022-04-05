05154a04737a1b8ed8e03d0ea3b8b97d9171e2aa
Akinbulejo Samson <akinbulejosamson77@gmail.com>
2022-04-04 17:02:03 -0700

    added Task 1

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,33 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * _strdup- returns a pointerto newly allocated
+ * @str: pointer to str to duplicate
+ *
+ * Return: NULL ifstr is NULL and insifficient memory
+ */
+
+char *_strdup(char *str)
+{
+	char *nstr;
+	unsigned int len, i;
+
+	if (str == NULL)
+		return (NULL);
+
+	len = 0;
+	while (str[len] != '\0')
+		len++;
+
+	nstr = malloc(sizeof(char) * (len + 1));
+
+	if (nstr == NULL)
+		return (NULL);
+
+	for (i = 0; i < len; i++)
+		nstr[i] = str[i];
+
+	nstr[len] = '\0';
+
+	return (nstr);
+}
